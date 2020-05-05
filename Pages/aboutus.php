d65485860477dccb17daecc4b3e86a47cff1af2a
Um-e-Hani <45230898+Um-e-Hani@users.noreply.github.com>
2020-05-05 21:26:26 +0500

    Create aboutus.php

---
+++ Pages/aboutus.php
@@ -0,0 +1,18 @@
+<html>
+    <head>
+        <link href='https://fonts.googleapis.com/css?family=Buenard' rel='stylesheet'>
+        <link rel="stylesheet" type="text/css" href="./css file.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+        <title>Login</title>
+    </head>
+
+    <body>
+        <!--html for page-->
+        <div class="c- login_container flexwrap" style="background-image:url(https://allegromediamusic.files.wordpress.com/2018/03/8281fadb7336e7ced706e1043e023e1c.jpg)">
+            <div class="c-8 c-m-12 wrap_aboutus flexwrap">
+                <div>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
