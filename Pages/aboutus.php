c3ce066e04ac051334d128088cfc428d4bf08916
Um-e-Hani <45230898+Um-e-Hani@users.noreply.github.com>
2020-05-05 23:22:30 +0500

    correcting responsiveness

--- Pages/aboutus.php
+++ Pages/aboutus.php
@@ -3,15 +3,16 @@
         <link href='https://fonts.googleapis.com/css?family=Buenard' rel='stylesheet'>
         <link rel="stylesheet" type="text/css" href="./css file.css">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-        <title>Login</title>
+        <title>About Us</title>
     </head>
 
     <body>
         <!--html for page-->
-        <div class="c- login_container flexwrap" style="background-image:url(https://allegromediamusic.files.wordpress.com/2018/03/8281fadb7336e7ced706e1043e023e1c.jpg)">
-            <div class="c-8 c-m-12 wrap_aboutus flexwrap">
-                <div>
-                </div>
+        <div class="c- login_container" style="filter: brightness(80%);position: absolute; top: 0px; left: 0px;right:0px; background-image:url(https://allegromediamusic.files.wordpress.com/2018/03/8281fadb7336e7ced706e1043e023e1c.jpg)">
+        </div>
+        <div class="c-aboutdesk-9 c-aboutsub-9 wrap_aboutus">
+            <div class="c-" style="padding:3%;">
+                <h1 class="form-title" style="font-size:45px; text-align:left">About Us</h1>
             </div>
         </div>
     </body>
