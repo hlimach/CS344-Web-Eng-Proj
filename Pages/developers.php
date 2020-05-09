94bcfb4b7357e5d35dcd9a7382daa0db7407c7d8
Um-e-Hani <45230898+Um-e-Hani@users.noreply.github.com>
2020-05-09 05:26:15 +0500

    developer page

---
+++ Pages/developers.php
@@ -0,0 +1,177 @@
+<html>
+    <head>
+        <link href='https://fonts.googleapis.com/css?family=Buenard' rel='stylesheet'>
+        <link rel="stylesheet" type="text/css" href="../Style/css file.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+        <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+	    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	    <link rel="stylesheet" href="../Style/Header_Footer_Style.css" type="text/css">
+        <title>Developers</title>
+    </head>
+
+    <body>
+        <!--html for page-->
+        <div class="c- aboutus_container devpage flexwrap">
+        </div>
+
+        <div class="c-aboutdesk-d wrap_aboutus">
+            <div >
+                <div id='sliding-menu' class='col-3 col-t-5 col-m-5'>
+		            <div id='slider-menu-exit' class="fa fa-times w3-large"></div>
+		            <div id='movie-menu'>
+			            <a href='#'>Movies</a><i class='fa fa-angle-down w3-large'></i>
+			            <ul>
+				            <li><a href='#'>TRENDING</a></li>
+				            <li><a href='#'>GENRE</a></li>
+				            <li><a href='#'>TOP RATED</a></li>
+				            <li><a href='#'>LATEST</a></li>
+			            </ul>
+		            </div>
+		            <div id='tv-show-menu'>
+			            <a href='#'>TV Shows</a><i class='fa fa-angle-down w3-large'></i>
+			            <ul>
+				            <li><a href='#'>TRENDING</a></li>
+				            <li><a href='#'>GENRE</a></li>
+				            <li><a href='#'>TOP RATED</a></li>
+				            <li><a href='#'>LATEST</a></li>
+			            </ul>
+		            </div>
+		            <div id='popular-menu'><a href='#'>Popular</a></div>
+	            </div>
+
+	            <div id='header' style="background-color:#000000">
+		            <div id='large-header' class='col-12 remove-t remove-m'>
+			            <div class='slider-menu col-1 fa fa-bars w3-large'></div>
+			            <div id='logo-large' class='col-3'>
+				            <a href='#'><img src='../Style/Logo_Large_Final.png' height='80'></a>
+			            </div>
+
+			            <!-- check for logged in/ out user -->
+			            <div class='user-icon col-1'><a href='#' class='fa fa-user-circle  w3-large'></a></div>
+			
+			            <!-- add functionality to search bar -->
+			            <div id='fixed-search-bar' class="col-4">
+		                    <form action="#">
+		                        <input id='search-bar' type='text' placeholder='Search...'/>
+					            <button id='search-btn' class='fa fa-search'/>
+		                    </form>
+		                </div>
+		            </div>
+                </div>
+
+		        <div id='small-header' class='remove col-t-12 col-m-12' style="background-color:#000000">
+			        <div class='slider-menu col-t-1 col-m-1 fa fa-bars w3-large'></div>
+			        <div id='logo-small' class='col-t-1 col-m-1'>
+				        <a href='#'><img src='../Style/Logo_Small_Final.png' height='60'></a>
+			        </div>
+
+			        <!-- check for logged in/ out user -->
+			        <div class='user-icon col-t-1 col-m-1'>			
+				        <a href='#' class='fa fa-user-circle w3-large'></a>
+			        </div>
+
+			        <div id='open-search-bar' class="col-t-1 col-m-1">
+				        <a href='#' class='fa fa-search  w3-large'></a>
+			        </div>
+
+			        <!-- add functionality to search bar -->
+		            <div id='hidden-search-bar' class="col-t-12 col-m-12">
+		                <form action="#">
+		                    <input id='search-bar-s' type='text' placeholder='Search...'/>
+					        <button id='search-btn-s' class='fa fa-arrow-right'/>
+		                </form>
+		            </div>
+		        </div>
+	        </div>
+
+            <div id="body">
+                <div class="c- paddingaboutus-d">
+                    <p class="aboutus-title"><b>Developers</b></p>
+                </div>
+                <div class="c- paddingdivs-d">
+                    <p class="aboutus-title" style="font-size:26px">Track Time has a developing team of highly skilled and professional web developers and coders:</p>
+                    <p class="aboutus-title " style="font-size:24px;">Maira Tariq</p>
+                    <p class="aboutus-title " style="font-size:24px">Haleema Ramzan</p>
+                    <p class="aboutus-title " style="font-size:24px">Maha Shahwar</p>
+                    <p class="aboutus-title " style="font-size:24px">Um E Hani</p>
+                </div>
+                <div style="height:100px">
+                    <div class="skewdiv" style="height:80px"> 
+                        <p class="form-title paddingdivs-d" style="font-size:36px;"><b>Maira Tariq</b></p>
+                    </div>
+                </div>
+                <div class="c- paddingaboutus-d" style="padding-bottom:2%">
+                    <p class="aboutus-title" style="font-size:23px">Maira Tariq possesses the ability to derive unique insights into any design challenge. She assists our clients in maximizing the value of their online presence.<br><br> She has been an invited speaker on the topic of web design industry for IEEE and has also given lectures to students at the annual web development fair. </p>
+                </div>
+                <div style="height:100px">
+                    <div class="skewdiv" style="height:80px"> 
+                        <p class="form-title paddingdivs-d" style="font-size:36px;"><b>Haleema Ramzan</b></p>
+                    </div>
+                </div>
+                <div class="c- paddingaboutus-d" style="padding-bottom:2%">
+                    <p class="aboutus-title" style="font-size:23px">Haleema Ramzan has been working with the IT industry for over 3 years, specializing in web for over 2. She has received her degree in Computer Science from the National University of Science and Technology,Pakistan.<br><br> She has  also previously held management positions at previous web firms before applying to Track TV.</p>
+                </div>
+                <div style="height:100px">
+                    <div class="skewdiv" style="height:80px"> 
+                        <p class="form-title paddingdivs-d" style="font-size:36px;"><b>Maha Shahwar</b></p>
+                    </div>
+                </div>
+                <div class="c- paddingaboutus-d" style="padding-bottom:2%">
+                    <p class="aboutus-title" style="font-size:23px">Maha Shahwar is passionate about web standards. With a BSCS degree, she sets out to learn as much as she can with the goal of creating ground breaking websites with positive user experiences.<br><br>Maha enjoys showing her potential and is also a co-founder of one of our other projects, Happy Times.</p>
+                </div>
+                <div style="height:100px">
+                    <div class="skewdiv" style="height:80px"> 
+                        <p class="form-title paddingdivs-d" style="font-size:36px;"><b>Um E Hani</b></p>
+                    </div>
+                </div>
+                <div class="c- paddingaboutus-d" style="padding-bottom:2%">
+                    <p class="aboutus-title" style="font-size:23px">Track Times youngest member, Um E Hani is a recent graduate from NUST, Pakistan with a major in CS, concentrating in Graphic Design.<br><br>With her background in fine arts and design, Hani is a quick learner with endless creative energy. During her time in Track TV, she has done it all, from branding and design to production and coding.</p>
+                </div>
+                <div class="c- paddingaboutus-d" >
+                    <h3 class="highlight form-title" style="padding-bottom:50px">And our teams welcomes you to Track TV!</h3>
+                </div>
+                <div class="colorstrip">
+                    <div class="skewdiv" style="height: 50px;"> 
+                    </div>
+                </div>
+                <div class="colorstrip">
+                    <div class="skewdiv" style="height: 50px;background-color:#333333"> 
+                    </div>
+                </div>
+
+                <div class="gifcontainer" style="padding:1%">
+                    <div class="skewdiv" style="height: 340px;background-image:url(https://i.pinimg.com/originals/b9/7d/c2/b97dc288d71e7938c1ce8b7faacdc9ac.gif);background-size:100% 100%;"> 
+                    </div>
+                </div>
+            </div>
+            <div id="bodyfooter" style="padding-bottom:30px;" >
+                <!--footer final, dont touch below!! -->
+	            <div class='footer'>
+		
+		            <div class='footer-heading'>Questions?</div>
+		            <div class='footer-content'><a href='#'>Services</a></div>
+		            <div class='footer-content'><a href='#'>FAQs</a></div>
+		            <div class='footer-content'><a href='#'>Contact Us</a></div>
+		            <div class='footer-content'><a href='#'>Developers</a></div>
+		            <div class='footer-heading'>Find Us</div>
+		            <div class='footer-heading' id='find-us-icons'>
+			            <a href='https://www.youtube.com' target='_blank' class='fa fa-youtube'></a>
+			            <a href='https://www.twitter.com' target='_blank' class='fa fa-twitter'></a>
+			            <a href='https://www.facebook.com' target='_blank' class='fa fa-facebook'></a>
+			            <a href='https://www.instagram.com' target='_blank' class='fa fa-instagram'></a>
+		            </div>
+		            <div id='footer-end' class='footer-heading'> CS-344 Web Engineering, Spring 2020: Semester Project.</div>
+	            </div>
+	            <!--footer final, dont touch above!! -->
+            </div>
+        </div>
+            
+       
+        
+
+	    <script src="../Functionality/jquery-3.2.1.js"></script>
+	    <script src="../Functionality/Header_Footer.js" type="text/javascript">
+		    //took it from here
+	    </script>
+    </body>
+</html>
