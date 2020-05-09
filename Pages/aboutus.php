915e52736e3e564f57726bd4590c55c493351b1b
Um-e-Hani <45230898+Um-e-Hani@users.noreply.github.com>
2020-05-09 05:27:36 +0500

    Complete Aboutus

--- Pages/aboutus.php
+++ Pages/aboutus.php
@@ -1,45 +1,165 @@
 <html>
     <head>
         <link href='https://fonts.googleapis.com/css?family=Buenard' rel='stylesheet'>
-        <link rel="stylesheet" type="text/css" href="./css file.css">
+        <link rel="stylesheet" type="text/css" href="../Style/css file.css">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+        <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+	    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	    <link rel="stylesheet" href="../Style/Header_Footer_Style.css" type="text/css">
         <title>About Us</title>
     </head>
 
     <body>
         <!--html for page-->
-        <div class="c- aboutus_container flexwrap">
+        <div class="c- aboutus_container aboutuspage flexwrap">
         </div>
-        <div class="c-aboutdesk-d d0h wrap_aboutus ">
-            <div class="c- paddingaboutus-d">
-                <p class="aboutus-title"><b>About Us</b></p>
-            </div>
-            <div class="c- paddingdivs-d">
-                <p class="aboutus-title" style="font-size:26px">We are a global community of tv lovers tracking their shows, reacting with others and sharing the moments that move us.</p>
-            </div>
-            <div class="gifcontainer">
-                <div class="skewdiv skewr" style="top: 400px;height: 150px;"> 
+            
+        <div class="c-aboutdesk-d wrap_aboutus">
+            <div>
+                <div id='sliding-menu' class='col-3 col-t-5 col-m-5'>
+		            <div id='slider-menu-exit' class="fa fa-times w3-large"></div>
+		            <div id='movie-menu'>
+			            <a href='#'>Movies</a><i class='fa fa-angle-down w3-large'></i>
+			            <ul>
+				            <li><a href='#'>TRENDING</a></li>
+				            <li><a href='#'>GENRE</a></li>
+				            <li><a href='#'>TOP RATED</a></li>
+				            <li><a href='#'>LATEST</a></li>
+			            </ul>
+		            </div>
+		            <div id='tv-show-menu'>
+			            <a href='#'>TV Shows</a><i class='fa fa-angle-down w3-large'></i>
+			            <ul>
+				            <li><a href='#'>TRENDING</a></li>
+				            <li><a href='#'>GENRE</a></li>
+				            <li><a href='#'>TOP RATED</a></li>
+				            <li><a href='#'>LATEST</a></li>
+			            </ul>
+		            </div>
+		            <div id='popular-menu'><a href='#'>Popular</a></div>
+	            </div>
+
+	            <div id='header' style="background-color:#000000">
+		            <div id='large-header' class='col-12 remove-t remove-m'>
+			            <div class='slider-menu col-1 fa fa-bars w3-large'></div>
+			            <div id='logo-large' class='col-3'>
+				            <a href='#'><img src='../Style/Logo_Large_Final.png' height='80'></a>
+			            </div>
+
+			            <!-- check for logged in/ out user -->
+			            <div class='user-icon col-1'><a href='#' class='fa fa-user-circle  w3-large'></a></div>
+			
+			            <!-- add functionality to search bar -->
+			            <div id='fixed-search-bar' class="col-4">
+		                    <form action="#">
+		                        <input id='search-bar' type='text' placeholder='Search...'/>
+					            <button id='search-btn' class='fa fa-search'/>
+		                    </form>
+		                </div>
+		            </div>
                 </div>
-                <div >
-                    <img class="gif" src="https://i.gifer.com/OaH.gif" />
+
+		        <div id='small-header' class='remove col-t-12 col-m-12' style="background-color:#000000">
+			        <div class='slider-menu col-t-1 col-m-1 fa fa-bars w3-large'></div>
+			        <div id='logo-small' class='col-t-1 col-m-1'>
+				        <a href='#'><img src='../Style/Logo_Small_Final.png' height='60'></a>
+			        </div>
+
+			        <!-- check for logged in/ out user -->
+			        <div class='user-icon col-t-1 col-m-1'>			
+				        <a href='#' class='fa fa-user-circle w3-large'></a>
+			        </div>
+
+			        <div id='open-search-bar' class="col-t-1 col-m-1">
+				        <a href='#' class='fa fa-search  w3-large'></a>
+			        </div>
+
+			        <!-- add functionality to search bar -->
+		            <div id='hidden-search-bar' class="col-t-12 col-m-12">
+		                <form action="#">
+		                    <input id='search-bar-s' type='text' placeholder='Search...'/>
+					        <button id='search-btn-s' class='fa fa-arrow-right'/>
+		                </form>
+		            </div>
+		        </div>
+	        </div>
+            <div id="body">
+                <div class="c- paddingaboutus-d">
+                    <p class="aboutus-title"><b>About Us</b></p>
+                </div>
+                <div class="c- paddingdivs-d">
+                    <p class="aboutus-title" style="font-size:26px">We are a global community of tv lovers tracking their shows, reacting with others and sharing the moments that move us.</p>
+                </div>
+                <div class="gifcontainer">
+                    <div class="skewdiv skewr" style="top: 450px;height: 150px;"> 
+                    </div>
+                    <div >
+                        <img class="gif" src="https://i.gifer.com/OaH.gif" />
+                    </div>
+                </div>
+                <div class="c- paddingaboutus-d">
+                    <p class="aboutus-title" style="font-size:23px" >It's Track TV time ... Friends' is on. <br> <br> It's that shared love of watching TV. Never missing an episode. Reacting to moments with tears, laughter and intrigue. Sitting with others, turning to them and saying at exactly the same time - 'Wait, what? Chandler and Rachel?' <br><br>
+                    Then the world changed. Peoples watching habits became fragmented, different. The TV universe got bigger ... and yet felt smaller at the same time. Mobile phones, tablets and other devices allowed us to watch from anywhere. Entire series were released in a day. 10 channels became 1,000. TV just kept getting better, and bigger, and better. And more than ever, we wanted to stay on top of our favorite shows. We got enraged when anyone even hinted at a spoiler. And finding our tribes, those like us, to share moments and react to, at the moment we felt it, became so vital, so enriching, so core to our daily routines.<br><br>
+                    And so we created our own multi-platform, virtual TV tracking community of millions so that we'd never lose, only grow, that emotional pull we shared and loved when we first heard those words - It's Track TV Time ... 'Friends' is on.</p>
+                </div>
+                <div class="c- paddingaboutus-d" >
+                    <h3 class="highlight form-title" style="padding-bottom:50px">Your entertainment is our number 1 priority!</h3>
+                </div>
+                <div style="height:150px">
+                    <div class="skewdiv skewr" style="height: 50px;"> 
+                    </div>
+                    <div class="skewdiv skewl" style="height: 50px;background-color:#333333"> 
+                    </div>
+                </div>
+                <div class="c- paddingaboutus-d">
+                    <p class="form-title highlight" style="font-size:40px; padding:0%">Meet the Team!</p>
+                    <p class="aboutus-title tcenter" style="font-size:30px;">Maira Tariq</p>
+                    <p class="aboutus-title tcenter" style="font-size:30px">Haleema Ramzan</p>
+                    <p class="aboutus-title tcenter" style="font-size:30px">Maha Shahwar</p>
+                    <p class="aboutus-title tcenter" style="font-size:30px">Um E Hani</p>
+                </div>
+                <div class="colorstrip">
+                    <div class="skewdiv" style="height: 50px;"> 
+                    </div>
+                </div>
+                <div class="colorstrip">
+                    <div class="skewdiv" style="height: 50px;background-color:#333333"> 
+                    </div>
+                </div>
+
+                <div class="gifcontainer" style="padding:1%">
+                    <div class="skewdiv" style="height: 340px;background-image:url(https://media1.tenor.com/images/034a997458e9317fcd47defb395e9ed6/tenor.gif?itemid=4713676);background-size:100% 100%;"> 
+                    </div>
                 </div>
             </div>
-            <div class="c- paddingaboutus-d">
-                <p class="aboutus-title" style="font-size:23px" >It's Track TV time ... Friends' is on. <br> <br> It's that shared love of watching TV. Never missing an episode. Reacting to moments with tears, laughter and intrigue. Sitting with others, turning to them and saying at exactly the same time - 'Wait, what? Chandler and Rachel?' <br><br>
+        
+        <div id="bodyfooter" style="padding-bottom:30px">    
+            <!--footer final, dont touch below!! -->
+	        <div class='footer'>
+		
+		        <div class='footer-heading'>Questions?</div>
+		        <div class='footer-content'><a href='#'>Services</a></div>
+		        <div class='footer-content'><a href='#'>FAQs</a></div>
+		        <div class='footer-content'><a href='#'>Contact Us</a></div>
+		        <div class='footer-content'><a href='#'>Developers</a></div>
+	
+		        <div class='footer-heading'>Find Us</div>
+		        <div class='footer-heading' id='find-us-icons'>
+			        <a href='https://www.youtube.com' target='_blank' class='fa fa-youtube'></a>
+			        <a href='https://www.twitter.com' target='_blank' class='fa fa-twitter'></a>
+			        <a href='https://www.facebook.com' target='_blank' class='fa fa-facebook'></a>
+			        <a href='https://www.instagram.com' target='_blank' class='fa fa-instagram'></a>
+		        </div>
 
-Then the world changed. Peoples watching habits became fragmented, different. The TV universe got bigger ... and yet felt smaller at the same time. Mobile phones, tablets and other devices allowed us to watch from anywhere. Entire series were released in a day. 10 channels became 1,000. TV just kept getting better, and bigger, and better. And more than ever, we wanted to stay on top of our favorite shows. We got enraged when anyone even hinted at a spoiler. And finding our tribes, those like us, to share moments and react to, at the moment we felt it, became so vital, so enriching, so core to our daily routines.<br><br>
+		        <div id='footer-end' class='footer-heading body'> CS-344 Web Engineering, Spring 2020: Semester Project.</div>
+	        </div>
+	        <!--footer final, dont touch above!! -->
+        </div>  
+    </div
 
-And so we created our own multi-platform, virtual TV tracking community of millions so that we'd never lose, only grow, that emotional pull we shared and loved when we first heard those words - It's Track TV Time ... 'Friends' is on.</p>
-            </div>
-            <div class="c- form-title">
-                <h3>Your entertainment is our number 1 priority!</h3>
-            </div>
-            <div class="gifcontainer">
-                <div class="skewdiv skewl" style="height: 450px;background-image:url(https://i.pinimg.com/originals/2c/f9/78/2cf97842dac30ace0c49a6ddb1c9ceca.gif);background-size:100% 100%;"> 
-
-                </div>
-            </div>
-        </div>
-
+	    <script src="../Functionality/jquery-3.2.1.js"></script>
+	    <script src="../Functionality/Header_Footer.js" type="text/javascript">
+		    //took it from here
+	    </script>
     </body>
 </html>
