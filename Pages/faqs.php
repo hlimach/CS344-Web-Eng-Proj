4a1d1b12247ac50c696df69bba4c1855095b9a8c
Um-e-Hani <45230898+Um-e-Hani@users.noreply.github.com>
2020-05-13 13:41:46 +0500

    faq page

---
+++ Pages/faqs.php
@@ -0,0 +1,86 @@
+<html>
+    <head>
+        <link href='https://fonts.googleapis.com/css?family=Buenard' rel='stylesheet'>
+        <link rel="stylesheet" type="text/css" href="../Style/css file.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+        <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+	    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	    <link rel="stylesheet" href="../Style/Header_Footer_Style.css" type="text/css">
+        <title>FAQs</title>
+    </head>
+
+    <body>
+        <!--html for page-->
+        <div class="c- aboutus_container faqpage flexwrap">
+        </div>
+
+        <div class="c-aboutdesk-d wrap_aboutus">
+            <div style="background-color:black">
+                <?php include 'header.html' ?>
+	        </div>
+
+            <div id="body">
+                <div class="c- paddingaboutus-d">
+                    <p class="aboutus-title"><b>FAQs</b></p>
+                </div>
+                <div class="c- paddingdivs-d">
+                    <p class="aboutus-title" style="font-size:26px">Confused about Track Time? Don't worry! We are here to help you!</p>
+                </div>
+                <div style="height:100px">
+                    <div class="skewdiv" style="height:80px"> 
+                        <p class="form-title paddingdivs-d" style="font-size:36px;"><b>How do i create my account?</b></p>
+                    </div>
+                </div>
+                <div class="c- paddingaboutus-d" style="padding-bottom:2%">
+                    <p class="aboutus-title" style="font-size:23px">Track TV allows free account creation. Just go up to the signup page and enter your information for instant account creation. Users are not allowed to reuse an email or to duplicate a username. Creation of a Track TV account gives you unlimited access to all our services.</p>
+                </div>
+                <div style="height:100px">
+                    <div class="skewdiv" style="height:80px"> 
+                        <p class="form-title paddingdivs-d" style="font-size:36px;"><b>What is a watchlist?</b></p>
+                    </div>
+                </div>
+                <div class="c- paddingaboutus-d" style="padding-bottom:2%">
+                    <p class="aboutus-title" style="font-size:23px">When ever you find a show that is to your liking, you add it in a watch later section that allows you to easily look it up again later on. This helps you remember your favourite shows and keep track of their episodes.</p>
+                </div>
+                <div style="height:100px">
+                    <div class="skewdiv" style="height:80px"> 
+                        <p class="form-title paddingdivs-d" style="font-size:36px;"><b>What is the following option?</b></p>
+                    </div>
+                </div>
+                <div class="c- paddingaboutus-d" style="padding-bottom:2%">
+                    <p class="aboutus-title" style="font-size:23px"> Track TV community gives you an oportunity to interact with other users as well. By following someone you can view their profile page. Similarly, if someone follows you, they  can see your profile page.</p>
+                </div>
+                <div style="height:100px">
+                    <div class="skewdiv" style="height:80px"> 
+                        <p class="form-title paddingdivs-d" style="font-size:36px;"><b>What to do if you need help?</b></p>
+                    </div>
+                </div>
+                <div class="c- paddingaboutus-d" style="padding-bottom:2%">
+                    <p class="aboutus-title" style="font-size:23px">Track TV community is always here to help you 24/7. Visit our contact us page and get help relating anything that is troubling you.</p>
+                </div>
+                <div class="c- paddingaboutus-d" >
+                    <h3 class="highlight form-title" style="padding-bottom:50px">Have fun at Track TV!</h3>
+                </div>
+                <div class="colorstrip">
+                    <div class="skewdiv" style="height: 50px;"> 
+                    </div>
+                </div>
+                <div class="colorstrip">
+                    <div class="skewdiv" style="height: 50px;background-color:#333333"> 
+                    </div>
+                </div>
+            </div>
+            <div id="bodyfooter" style="padding-bottom:30px;" >
+                <?php include 'footer.html' ?>
+            </div>
+        </div>
+            
+       
+        
+
+	    <script src="../Functionality/jquery-3.2.1.js"></script>
+	    <script src="../Functionality/Header_Footer.js" type="text/javascript">
+		    //took it from here
+	    </script>
+    </body>
+</html>
