4a35cc4922937c1075949ea8e6c6ee636ca3d9d8
MairaTariq16 <55734983+MairaTariq16@users.noreply.github.com>
2020-05-14 02:18:27 +0500

    Update user-followers.php

--- Pages/user-followers.php
+++ Pages/user-followers.php
@@ -1,53 +1,58 @@
 <?php 
-
+//display user info and header
 include 'userpage_top.php';
 include 'dbconnect.php';
 
 
-$sql='select followerID from followers where user=2 ';
+$sql='select followerID from followers where user=1 ';
 $result=mysqli_query($conn, $sql);
 
-echo  "<h2>Followers</h2>";
-
-if ($result->num_rows > 0) {
-    echo "<ul id='user-movie-list'>";
+echo  "<h3>Followers (".mysqli_num_rows($result).")</h3>";
+    if ($result->num_rows > 0) {
+     echo "<ul id='user-list'>";
     
-// loop through all watched movie row
-while($row = $result->fetch_assoc()) {
-//$userid=$row["user"];
-$userID=$row["followerID"];
 
-$sql2='select * from user where userID='.$userID;
-$result2=mysqli_query($conn,$sql2);
-if ($result2->num_rows > 0)
-{
-    while($row=$result2->fetch_assoc())
+        while($row = $result->fetch_assoc()) 
+        {
+        $userID=$row["followerID"];
+
+            $sql2='select * from user where userID='.$userID;
+            $result2=mysqli_query($conn,$sql2);
+            if ($result2->num_rows > 0)
+            {
+                while($row=$result2->fetch_assoc())
+                
+                {
+                $fname=$row["f_name"];
+                $lname=$row["l_name"];
+                $pic=$row["pic_url"];
+
+            echo "<li>
+            <a id='list-link' href='userhome.php?id=$userID'>
+                <div id=\"user-follow-list\">
+                <img id=\"list-movie-poster\" class='follower-pic' src=\"".$pic."\">
+                <h3>".$fname.' '.$lname."</h3>
+                </div>
+                </li>
+                </a>";
+                
+                }//end of inner while
+
+            }//end of inner if
     
-    {
-    $fname=$row["f_name"];
-    $lname=$row["l_name"];
-    $pic=$row["pic_url"];
+        }//end of outer while
 
-echo "<li>
-    <div id=\"user-follow-list\">
-    <img id=\"list-movie-poster\" class='follower-pic' src=\"".$pic."\">
-    <h3>".$fname.' '.$lname."</h3>
-    </div>
+    echo "</ul>";
 
-    </li>";
-    
-}
+    }//end if outer if
 
-}
-    
-}
-echo "</ul>";
-}
-else
-echo '<h4>NO MOVIES FOUND</h4>';
+    else
+    echo '<h6>0 FOLLOWERS</h6>';
+
+//close database connection    
 $conn -> close();
 
-
+//display footer
 include 'userpage_bottom.php';
 
 ?>
