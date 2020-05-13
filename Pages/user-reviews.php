e455eedd1b24fd1a356bbbb95f8c81b8d52a4cf9
MairaTariq16 <55734983+MairaTariq16@users.noreply.github.com>
2020-05-14 02:20:03 +0500

    Update user-reviews.php

--- Pages/user-reviews.php
+++ Pages/user-reviews.php
@@ -1,56 +1,70 @@
 <?php 
+
+//display user info
 include 'userpage_top.php';
 
 include 'dbconnect.php';
 
 
-$sql='select * from review where user=2;';
+$sql='select * from review where user=1;';
 $result=mysqli_query($conn, $sql);
 
-echo  "<h2>Reviews</h2>";
+echo  "<h3>Reviews (".mysqli_num_rows($result).")</h3>";
 
-if ($result->num_rows > 0) {
-    echo "<ul id='user-movie-list'>";
+    if ($result->num_rows > 0) {
+        echo "<ul id='user-list'>";
+        
     
-// loop through all watched movie row
-while($row = $result->fetch_assoc()) {
-    
-    $movieID=$row["movie"];
-    $review_title=$row["title"];
-    $reviewtext=$row["review_text"];
+        while($row = $result->fetch_assoc()) {
+            
+            $movieID=$row["movie"];
+            $review_title=$row["title"];
+            $reviewtext=$row["review_text"];
+            $reviewtext= substr($reviewtext,0,120);
+            $reviewrate=$row["review_rating"];
 
-$sql2='select * from movie where movieid='.$movieID;
-$result2=mysqli_query($conn,$sql2);
-if ($result2->num_rows > 0)
-{
-    while($row=$result2->fetch_assoc())
-    
-    {
-    $title=$row["title"];
-    $poster=$row["poster_url"];
+            $sql2='select * from movie where movieid='.$movieID;
+            $result2=mysqli_query($conn,$sql2);
 
-echo "<li>
-    <div id=\"user-movie-info\">
-    <img id=\"list-movie-poster\" src=\"".$poster."\">
-    <h3>".$title."</h3>
-    <h4>".$review_title."</h4>
-    <div id='list-movie-desc'>
-    <p>"."\"".$reviewtext."\""."</p>
-    </div>
-    </div>
-    </li>";
-    
-}
+                if ($result2->num_rows > 0)
+                {
+                    while($row=$result2->fetch_assoc())
+                    
+                        {
+                        $id=$row["movieID"];
+                        $title=$row["title"];
+                        $poster=$row["image_url"];
 
-}
-    
-}
-echo "</ul>";
-}
+                    echo 
+                        "<li>
+                        <a id='list-link' href='search-result.php?$id'>
+                        <div id=\"user-movie-info\">
+                        <img id=\"list-movie-poster\" src=\"$poster\">
+                        <h3>$title</h3>
+                        <h6>\"$review_title\" (Rating:  $reviewrate/10)</h6>
+                        <div id='list-movie-desc'>
+                        <p>\"$reviewtext...\"</p>
+                        </div>
+                        </div>
+                        </a>
+                        </li>";
+                        
+                    }
+
+                }
+            
+        }
+
+    echo "</ul>";
+
+    }
 
 else
-    echo '<h4>NO REVIEWS FOUND</h4>';
+    echo '<h6>NO REVIEWS FOUND</h6>';
 
+    //close database connection
 $conn -> close();
+
+//display footer
  include 'userpage_bottom.php';
   ?>
