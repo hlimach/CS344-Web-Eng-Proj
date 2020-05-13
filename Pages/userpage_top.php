f15a6a83fe160bcdf8156daa068ec90c4d6d3a15
MairaTariq16 <55734983+MairaTariq16@users.noreply.github.com>
2020-05-14 02:21:54 +0500

    Update userpage_top.php

--- Pages/userpage_top.php
+++ Pages/userpage_top.php
@@ -3,27 +3,37 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>User Homepage</title>
-    <link rel="stylesheet" href="maira.css" type="text/css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <link rel="stylesheet" href="../Style/UserPage_Style.css" type="text/css">
+
 </head>
 
 <body id='user-homepage'>
-<div class='header' class='col-12 col-m-12'></div>
-<div id='breadcrumb-menu' class='col-12 col-m-12'></div>
-<div id='main-content' class='col-12 col-m-12'>
-    <div id='user-info' class='col-3 col-m-3 '>
+
+<?php include "../Pages/header.html" ?>
+
+<div class='col-12 col-t-12 col-m-12'>
+</div>
+
+<div id='main-content' class='col-12 col-t-12 col-m-12 '>
     
-    <?php include 'display-userinfo.php' ?>
+   
+    <h3 id='optionalstuff'>Maira's Homepage</h3>
+    <div id='user-info' class='col-3 col-t-12 col-m-12'>
+    
+    <?php
+
+     include '../Pages/display-userinfo.php'
+      ?>
   
 
     </div>
-    <div class="vl" id='optionalstuff'></div>
-    <div id='user-links' class='col-8 col-m-8 col-mo-12'>
+    
+    <div id='user-links' class='col-8 col-t-12 col-m-12'>
        <div id='user-links-nav' class='nav'>
-           <div><a id='watched' href="userhome.php"> Watched Movies</a></div>
-           <div><a id='fav' href="user-favs.php"> Favorites</a></div>
-           <div><a id='later' href="user-later.php">Watch Later</a></div>
-           <div><a id='reviews' href="user-reviews.php">Reviews</a></div>
+           <div><a id='watched' href="userhome.php"> Watched Movies </a></div>
+           <div><a id='fav' href="user-favs.php"> Favorites </a></div>
+           <div><a id='later' href="user-later.php">Watch Later </a></div>
+           <div><a id='reviews' href="user-reviews.php">Reviews </a></div>
        </div>
       
       <div id='user-movie-content'>
