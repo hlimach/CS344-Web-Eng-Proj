c35e05d87b7348504c5c1f640086bc3851a2bb5d
MairaTariq16 <55734983+MairaTariq16@users.noreply.github.com>
2020-05-14 02:16:28 +0500

    Update display-userinfo.php

--- Pages/display-userinfo.php
+++ Pages/display-userinfo.php
@@ -1,20 +1,21 @@
 <?php
-
+//to get user's info from database and display in left section of the page
 include 'dbconnect.php';
-        //get and display data of the user from mysql database  
-       
-                $sql='select * from followers where user=2';
+         
+                //get number of followers and following
+                $sql='select * from followers where user=1';
                 $result=mysqli_query($conn, $sql);
                 $followers= mysqli_num_rows($result);
             
                
-                $sql='select * from followers where followerID=2';
+                $sql='select * from followers where followerID=1';
                 $result=mysqli_query($conn,$sql);
                 $following= mysqli_num_rows($result);
                 
-                $sql='select * from user where userID=2';
-                $result=mysqli_query($conn,$sql);
 
+                //get user's info
+                $sql='select * from user where userID=1';
+                $result=mysqli_query($conn,$sql);
                 if ($result->num_rows > 0) {
 
                 $row = $result->fetch_assoc();
@@ -25,21 +26,22 @@ include 'dbconnect.php';
                 $bio=$row["bio"];
                 $age=$row["age"];
                
-                    echo '<div id="user-picture"><img id="profilepicture" src="'.$picture.'" alt=""></div>
-                    <div id="user-name">'.$fname." ".$lname.", ".$age.'</div>
-                    <div id="user-handle">'.'@'.$handle.'</div>
-                    <div id="user-following"><a href="user-followers.php">'.$followers.' Followers</a></div>
-                    <div id="user-following"><a href="user-following.php">'.$following.' Following </a></div>
+                    echo
+                   '<div id="user-picture" class="col-12 col-t-5"><img id="profilepicture" src="'.$picture.'" alt=""></div>
+                    <div id="user-name"  >'.$fname." ".$lname.", ".$age.'</div>
+                    <div id="user-handle"  >'.'@'.$handle.'</div>
+                    <div id="user-follow-div" class="col-12 col-t-7">
+                        <div id="user-following"><a href="user-followers.php">'.$followers.' Followers</a></div>
+                        <div id="user-following"><a href="user-following.php">'.$following.' Following </a></div>
+                    </div>
                     <div id="user-follow"><button class="follow-btn">Follow </button></div>
-                    <div id="user-bio">
+                    <div id="user-bio" class="col-12 col-t-12">
                         <h4>About Me</h4>
                         <p>'.$bio.'</p>
                     </div>
                     <div id="user-edit">
-                    <button id="user-edit-btn">Edit Profile</button>
+                        <button id="user-edit-btn">Edit Profile</button>
                     </div>';
-
                 }
                 
-
         ?>
