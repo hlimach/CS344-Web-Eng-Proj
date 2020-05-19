99396ffd6955f5763554e5df3bd1b67e11f00d4a
MairaTariq16 <55734983+MairaTariq16@users.noreply.github.com>
2020-05-19 14:21:25 +0500

    Functionality for follow button on user page

---
+++ Pages/follow.php
@@ -0,0 +1,36 @@
+<?php
+session_start();
+
+$state=$_GET["state"];
+$userID=$_GET["user"];
+include '../Pages/dbconnect.php';
+
+switch ($state) {
+    case 'Follow':
+        $sql='insert into followers(user,followerID) values ('.$userID.','.$_SESSION["userid"].')';
+        $result=mysqli_query($conn,$sql);
+        echo "Unfollow";
+        break;
+    
+    case 'Unfollow':
+        $sql="delete from followers where user=".$userID." and followerID=".$_SESSION['userid'];
+        $result=mysqli_query($conn,$sql);
+        echo "Follow";
+        break;
+    case ' ';
+        $sql="select user, followerid from followers where user=".$userID." and followerID=".$_SESSION['userid'];
+        $result=mysqli_query($conn,$sql);
+      
+        if(mysqli_num_rows($result) > 0)
+        {
+            printf('Unfollow');
+        }
+          else
+        {
+            echo "Follow";
+        }
+        break;
+}
+
+
+?>
