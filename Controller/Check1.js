2bc7b7852efea01999578d296d97a9f3a703d0d3
Um-e-Hani <45230898+Um-e-Hani@users.noreply.github.com>
2020-05-20 08:52:49 +0500

    Add files via upload

--- Controller/Check1.js
+++ Controller/Check1.js
@@ -1,43 +1,4 @@
-$(document).ready(function(){
 
-	$(window).scroll(function() {
-		if($(window).scrollTop() > 20)
-			$("#header").css({"background-color": "rgb(18,18,18)", "-webkit-transition": "background-color 400ms linear"});
-		else
-			$("#header").css({"background-color": "transparent", "-webkit-transition": "background-color 400ms linear"});
-	});
-
-	$('#open-search-bar').click(function(){
-		$('.slider-menu, #logo-small, .user-icon, #open-search-bar').css('display','none');
-		$('#hidden-search-bar').slideDown("slow");
-
-		$('#page-content').click(function(){
-			$('#hidden-search-bar').css('display','none');
-			$('.slider-menu, #logo-small, .user-icon, #open-search-bar').css('display','block');
-		});
-	});
-
-	//sliding menu functionality
-	$('.slider-menu').click(function(){
-		$('#sliding-menu').animate({"left":"0px"}, "slow").addClass('visible');
-		$('#header, #page-content, #footer').css('overflow','hidden');
-
-		$('#slider-menu-exit').click(function(){
-			$('#sliding-menu').animate({"left":"-1000px"}, "slow").removeClass('visible');
-			$('#header, #page-content, #footer').css('overflow','hide');
-		});
-
-	});
-
-	//open orclose submenus in slider menu
-	$('#movie-menu i.fa').click(function(){
-		$('#movie-menu li').slideToggle();
-	});
-
-	$('#tv-show-menu i.fa').click(function(){
-		$('#tv-show-menu li').slideToggle();			
-	});
-}); 
 		$(document).ready(function() {
             $("#button1").click(function(){
              document.location.href = '../View/EditUser.php';
