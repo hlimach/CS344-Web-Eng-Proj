de94367f13b158c2ff930daff2de971e59a57e1d
Um-e-Hani <45230898+Um-e-Hani@users.noreply.github.com>
2020-05-20 09:04:27 +0500

    Add files via upload

--- View/ContactUs.php
+++ View/ContactUs.php
@@ -1,6 +1,5 @@
 <?php 
     session_start();
-    include '../Controller/CheckLogin.php';
 ?>
 <html>
 <head>
--- View/Faqs.php
+++ View/Faqs.php
@@ -1,6 +1,6 @@
 <?php 
     session_start();
-    include '../Controller/CheckLogin.php';
+    
 ?>
 <html>
     <head>
