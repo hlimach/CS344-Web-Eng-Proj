3c04a8d3b3434c568f70beaec802f8be24ab2c16
Maha Shahwar <55735054+MahaShahwar@users.noreply.github.com>
2020-05-20 05:36:26 +0500

    Update Admin.php

--- View/Admin.php
+++ View/Admin.php
@@ -20,67 +20,91 @@
 	<div class="col-12">
 	    <?php include 'header.php' ?>
     </div>
-    <div class="col-12 col-t-12 col-m-12" style="display: inline-block;">
-	    <div><h2 id="h2" style="float: left;clear: left">Users</h2></div>
-	    <div style="clear: right"><h6 id="h3"><a href="./admin_delete_user.php"><input id="inputtext-admin" type="button" name="edit" value="Delete User by User Name"></h6></a></div>
-		</div>
-	    <div class="col-12 col-t-12 col-m-12 scrolladd">
-		    <?php
-		        $dbhost = 'localhost';
-                $dbuser = 'root';
-                $dbpass = '';
-                $dbname='movieswebsite';
-                $link = mysqli_init();
-                $conn = mysqli_connect($dbhost, $dbuser, $dbpass,$dbname);
-				$query="select * from user";
-                $result=mysqli_query($conn,$query);
-                while($row = mysqli_fetch_array($result))
-                {
-                    $image=$row['pic_url'];
-                    $username=$row['username'];
-                    $bio=$row['bio'];
-                    $age=$row['age'];
-                    $mail=$row['email'];
-                    echo '
-                    <div id="scrollinner" class="user-movie-info-admin scrollinner-t scrollinner-m">
-                    <div class="col-12 col-t-12 col-m-12 ch1" style="background-color:black"><img src='.$image.' class="col-10 col-t-10 col-m-10" style="height:70%;border-radius:50%"></div>
-                    <div class="col-12 ch1 othertextd"><h5 style="margin-bottom:0px" class="ch">'.$username.'</h5><h5 style="margin-bottom:0px">'.$age.'</h5><p style="margin-top:0px;margin-bottom:0px" class="remove-t remove-m othertextd"> Email: '.$mail.'</p>
-                    <p style="margin-top:0px" class="remove-t remove-m othertextd">Description: '.$bio.'</p>
+   <div class="col-12 col-t-12 col-m-12" style="display: inline-block;">
+            <div><h2 id="h2" style="float: left;clear: left">Users</h2></div>
+            <div style="clear: right"><h6 id="h3"><input id="inputtext" type="button" name="edit" value="Delete User by id"></h6></div>
+        </div>
+    <div class="col-12 col-t-12 col-m-12 scrolladd">
+        <div class="col-12 col-t-12 col-m-12" style="text-align: center;">
+            <div class="col-1 col-t-1 col-m-1" style="float: left"><p>UserID</p></div>
+            <div class="col-2 col-t-2 col-m-2" style="float: left"><p>UserName</p></div>
+            <div class="col-2 col-t-2 col-m-2" style="float: left"><p>fname</p></div>
+            <div class="col-2 col-t-2 col-m-2" style="float: left;"><p>lname</p></div>
+            <div class="col-1 col-t-1 col-m-1" style="float: left;"><p>Age</p></div>
+            <div class="col-2 col-t-2 col-m-2" style="float: left;"><p>Gender</p></div>
+            <div class="col-2 col-t-2 col-m-2" style="float: left;"><p>Email</p></div>
+        </div>
+       
+        <?php
+        $dbhost = 'localhost';
+                    $dbuser = 'root';
+                     $dbpass = '';
+                     $dbname='movieswebsite';
+                     $link = mysqli_init();
+                     $conn = mysqli_connect($dbhost, $dbuser, $dbpass,$dbname);
+                     $query="select * from user";
+                      $result=mysqli_query($conn,$query);
+                      while($row = mysqli_fetch_array($result))
+                         {
+                            $userid=$row['userID'];
+                            $username=$row['username'];
+                            $fname=$row['f_name'];
+                            $lname=$row['l_name'];
+                            $gender=$row['gender'];
+                            $age=$row['age'];
+                            $mail=$row['email'];
+                            echo '
+                             <div class="col-12 col-t-12 col-m-12 ch1" style="background-color:black;text-align:center">
+                            <div class="col-1 col-t-1 col-m-1 ch1 othertextd" style="float:left"><p class="ch">'.$userid.'</p></div>
+                            <div class="col-2 col-t-2 col-m-2 ch1 othertextd" style="float:left"><p class="ch">'.$username.'</p></div>
+                            <div class="col-2 col-t-2 col-m-2 ch1 othertextd" style="float:left"><p class="ch">'.$fname.'</p></div>
+                            <div class="col-2 col-t-2 col-m-2 ch1 othertextd" style="float:left"><p class="ch">'.$lname.'</p></div>
+                            <div class="col-1 col-t-1 col-m-1 ch1 othertextd" style="float:left"><p class="ch">'.$age.'</p></div>
+                            <div class="col-2 col-t-2 col-m-2 ch1 othertextd" style="float:left"><p class="ch">'.$gender.'</p></div>
+                            <div class="col-2 col-t-2 col-m-2 ch1 othertextd" style="float:left"><p class="ch">'.$mail.'</p></div>
+                            </div>';
+                         }
 
-                    </div>
-                    <div class="sp"><input type="button" value="remove" id="removebutton" class="removebutton2"></div>
-                    </div>';
-                }
-		    ?>
-	    </div>
+        ?>
 
-    <div class="col-12 col-t-12 col-m-12" style="display: inline-block;padding-top: 50px">
-	    <div class="remove-m"><h2 id="h2" style="float: left;clear: left">Movies/Tv Shows</h2></div>
-	    <div class="remove-t remove"><h4 id="h2" style="float: left;clear: left">Movies/Tv Shows</h4></div>
-	    <div style="clear: right;"><h6 id="h3"><a href="./admin_delete_movie.php"><input id="inputtext-admin" type="button" name="edit" value="Delete by id"></a>
-	    <a href="./admin_new_movie.php"><input id="inputtext-admin" type="button" name="Delete" value="Add Movie"></a>
-	    <a href="./admin_edit_movie.php"><input id="inputtext-admin" type="button" name="Delete" value="Edit Movie"></a></h6></div>
     </div>
+<div class="col-12 col-t-12 col-m-12" style="display: inline-block;padding-top: 50px">
+    <div class="remove-m"><h2 id="h2" style="float: left;clear: left">Movies/Tv Shows</h2></div>
+    <div class="remove-t remove"><h4 id="h2" style="float: left;clear: left">Movies/Tv Shows</h4></div>
+    <div style="clear: right;"><h6 id="h3"><input id="inputtext" type="button" name="edit" value="Delete by id">
+    <input id="inputtext" type="button" name="Delete" value="Add Movie">
+    <input id="inputtext" type="button" name="Delete" value="Edit Movie"></h6></div>
+</div>
+    <div class="col-12 col-t-12 col-m-12 scrolladd" id="moviediv">
+        <div class="col-12" style="text-align: center;">
+            <div class="col-3 col-t-3 col-m-3" style="float: left"><p>movieID</p></div>
+            <div class="col-3 col-t-3 col-m-3" style="float: left"><p>Title</p></div>
+            <div class="col-3 col-t-3 col-m-3" style="float: left"><p>Rating</p></div>
+            <div class="col-3 col-t-3 col-m-3" style="float: left;"><p>Seasons</p></div>
+        </div>
+        <?php
+                    
+                     $query="select * from movie";
+                      $result=mysqli_query($conn,$query);
+                      while($row = mysqli_fetch_array($result))
+                         {
+                            $id=$row['movieID'];
+                            $title=$row['title'];
+                            $season=$row['seasons'];
+                            $rating=$row['rating'];
+                            echo '
+                            <div class="col-12 col-t-12 col-m-12 ch1" style="background-color:black;text-align:center">
+                            <div class="col-3 col-t-3 col-m-3 ch1 othertextd" style="float:left"><p class="ch">'.$id.'</p></div>
+                            <div class="col-3 col-t-3 col-m-3 ch1 othertextd" style="float:left"><p class="ch">'.$title.'</p></div>
+                            <div class="col-3 col-t-3 col-m-3 ch1 othertextd" style="float:left"><p class="ch">'.$season.'</p></div>
+                            <div class="col-3 col-t-3 col-m-3 ch1 othertextd" style="float:left"><p class="ch">'.$rating.'</p></div>
+                            </div>';
+                         }
 
-	<div class="col-12 col-t-12 col-m-12 scrolladd" id="moviediv">
-		<?php
-            $query="select * from movie";
-            $result=mysqli_query($conn,$query);
-            while($row = mysqli_fetch_array($result))
-            {
-                $image=$row['image_url'];
-                $title=$row['title'];
-                $season=$row['seasons'];
-                echo '
-                <div id="scrollinner" class="user-movie-info-admin scrollinner-t scrollinner-m" style="margin-left:30px">
-                <div class="col-12 col-t-12 col-m-12 ch1"><img src='.$image.' class="col-12 col-t-12 col-m-12" style="height:70%">
-                </div>
-                <div class="col-12 ch1" style="background-color:#303030"><h5 style="margin-bottom:0px;" class="remove-t remove-m othertextd ch2">'.$title.'</h5><p class="othertextd" style="margin-top:0px"> Seasons: '.$season.'</p></div>
-                <div class="sp"><input type="button" value="remove" id="removebutton" class="removebutton1"></div>
-                </div>';
-            }
-		?> 
-	</div>
+        ?>
+        </div>
+        <div class="col-12" style="text-align: center;color: white;margin-top: 30px"><button id="logout"><a href="logout.php" style="text-decoration: none: ">LOG OUT</a></button></div>
+
     <div style="display:inline-block;width:100%">
         <?php include 'footer.html'; ?>
     </div>
